37a67b6f0ccce549d2db4af6a3b3ada99dd8061e
IbragimKhaniev <newinterny@gmail.com>
2025-11-11 15:44:05 +0300

    Register Member and Ad models in Django admin

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Member, Ad
 
-# Register your models here.
+@admin.register(Member)
+class MemberAdmin(admin.ModelAdmin):
+    list_display = ('id', 'email', 'username', 'is_moderator', 'created_at')
+    search_fields = ('email', 'username')
+    list_filter = ('is_moderator',)
+
+@admin.register(Ad)
+class AdAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'owner', 'status', 'price', 'created_at')
+    search_fields = ('title', 'description', 'contacts')
+    list_filter = ('status',)
